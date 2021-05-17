ec2a1e645990fa9a2feb2480d156cc155ce7c412
Alexisvib <alexis.vib@gmail.com>
2021-05-17 17:19:57 +0200

    files for quest 03 - Symfony : Création de la page d’accueil

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,15 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProgramController extends AbstractController
+class DefaultController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("/", name="app_index")
      */
     public function index(): Response
     {
-        return $this->render('program/index.html.twig', [
-            'website' => 'Wild Séries',
-        ]);
+        return $this->render('index.html.twig');
     }
 }
