9ecbab41b5857a02cfb3cb6ef8ee27c74039c121
Alexisvib <alexis.vib@gmail.com>
2021-05-17 16:46:31 +0200

    add ProgramController

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProgramController extends AbstractController
+{
+    /**
+     * @Route("/programs/", name="program_index")
+     */
+    public function index(): Response
+    {
+        return new Response(
+            '<html><body>Wild Series Index</body></html>'
+        );
+    }
+}
